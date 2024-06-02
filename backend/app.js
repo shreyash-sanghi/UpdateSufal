66dd36b3136634226559cb46108d006ab9e1b586
shreyash jain <sheryash123jain@gmail.com>
2024-06-02 22:21:06 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const auth = require("./src/routers/auth")
 const events = require("./src/routers/events")
 const MyTeam = require("./src/routers/team")
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://sufal.vercel.app",
     methods:["POST", "GET", "PATCH", "PUT", "DELETE"],
     credential:true
 }))
