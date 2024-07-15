cd0a692ff7cc36e7ebeb8e4c97362a50db3f16ef
Vedansh Jain <vedansh.code@gmail.com>
2024-07-15 12:21:39 +0530

    yearly brochures added

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -4,6 +4,9 @@ import Button from '../components/Button';
 import { GrUserExpert } from "react-icons/gr";
 import Footer from '../components/Footer';
 import { useNavigate } from 'react-router-dom';
+import Carousel2 from '../components/Carousel2';
+
+
 import {ref,uploadBytes,getStorage ,getDownloadURL,deleteObject} from "firebase/storage";
 import team3 from '../assets/mam5.png';
 import { Link } from 'react-router-dom';
@@ -161,6 +164,49 @@ const Home = () => {
 			  verifytoken();
 		  }
 	  },[])
+
+	  const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		responsive: [
+		  {
+			breakpoint: 1024,
+			settings: {
+			  slidesToShow: 4,
+			  slidesToScroll: 1,
+			  infinite: true,
+			  dots: true
+			}
+		  },
+		  {
+			breakpoint: 768,
+			settings: {
+			  slidesToShow: 2,
+			  slidesToScroll: 1
+			}
+		  },
+		  {
+			breakpoint: 480,
+			settings: {
+			  slidesToShow: 1,
+			  slidesToScroll: 1
+			}
+		  }
+		]
+	  };
+	
+	  const images = [
+		"image1.jpg",
+		"image2.jpg",
+		"image3.jpg",
+		"image4.jpg",
+		"image5.jpg",
+		"image6.jpg"
+	  ];
+	
 	return (
 	       <>
 		   <Header/>
@@ -381,22 +427,7 @@ const Home = () => {
 				</div>
 			</div>
 
-			<main className="flex py-6 lg:py-14 pt-10 md:pt-6 bg-white  flex-1 w-full flex-col items-center justify-center text-center md:text-start px-4 sm:mt-12 mt-20">
-        <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal sm:text-5xl">
-          Our
-          <span className="relative whitespace-nowrap text-pink-700">
-            <svg
-              aria-hidden="true"
-              viewBox="0 0 418 42"
-              className="absolute top-2/3 left-0 h-[0.58em] w-full fill-yellow-300/90"
-              preserveAspectRatio="none"
-            >
-              <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path>
-            </svg>
-            <span className="relative ml-4">Memories</span>
-          </span>
-        </h1>
-      </main>
+			
     
 			{/* <div className="max-w-7xl mx-auto px-5 pt-6 flex flex-col items-start justify-start selection:bg-[#7fe0c662] gap-5">
 				
@@ -445,6 +476,7 @@ const Home = () => {
 			{/*  */}
 {/* <<<<<<< HEAD */}
 
+<Carousel2></Carousel2>
 			
 			<section class="text-gray-600">
   <div class="container mx-auto flex px-5 lg:px-24 py-10 md:py-20 md:flex-row flex-col items-center">
@@ -610,7 +642,7 @@ const Home = () => {
 				</div>
 			</div> */}
 			
-			<Glimpses></Glimpses>
+			{/* <Glimpses></Glimpses> */}
 			<div
 				id="team"
 				className="w-full max-w-7xl mx-auto px-3 pb-16 selection:bg-[#0a755862]"
