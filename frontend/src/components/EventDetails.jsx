88a969daef690844b23d786215fcd4ff4c3eacf6
Vedansh Jain <vedansh.code@gmail.com>
2024-07-24 20:43:50 +0530

    event page padding removed

--- frontend/src/components/EventDetails.jsx
+++ frontend/src/components/EventDetails.jsx
@@ -90,7 +90,7 @@ getdata();
     <>
       <Header />
       <section className="text-gray-600">
-        <div className="container mx-auto flex px-5 py-10 lg:py-16 md:flex-row flex-col items-center">
+        <div className="container mx-auto flex sm:px-5 py-10 lg:py-16 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <main className="overflow-x-hidden lg:py-10 z-10 w-full flex-col items-center justify-start sm:mt-0 py-10 ">
         <h1 className="mx-auto max-w-4xl text-gray-800 lg:text-start font-display text-5xl font-bold tracking-normal sm:text-5xl">
