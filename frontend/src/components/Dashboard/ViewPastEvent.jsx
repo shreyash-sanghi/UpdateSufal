39853cf3402bee35f5ea299724d83f9e92bc0370
shreyash jain <sheryash123jain@gmail.com>
2024-08-07 20:55:53 +0530

    edit chiefguest

--- frontend/src/components/Dashboard/ViewPastEvent.jsx
+++ frontend/src/components/Dashboard/ViewPastEvent.jsx
@@ -21,6 +21,7 @@ const ViewPastEvent = ()=>{
     EventBanner: "",
     Discreption: "",
     ImageName: "",
+    ChiefGuest: "",
   })
   const [EventImage,setEventImage] = useState([]);
   const [EventImageName,setEventImageName] = useState([]);
@@ -40,6 +41,7 @@ const ViewPastEvent = ()=>{
              EventBanner: url1,
             Discreption: result.Discreption, 
            ImageName: result.ImageName, 
+           ChiefGuest: result.ChiefGuest, 
             }
           )
       })
@@ -82,7 +84,7 @@ const ViewPastEvent = ()=>{
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <main className="overflow-x-hidden lg:py-10 z-10 w-full flex-col items-center justify-start sm:mt-0 py-10 ">
         <h1 className="mx-auto max-w-4xl text-gray-100 lg:text-start font-display text-5xl font-bold tracking-normal sm:text-5xl">
-          Mamta <br className='' />
+        <br className='' />
           <span className="relative whitespace-wrap text-pink-700">
             <svg
               aria-hidden="true"
@@ -92,7 +94,7 @@ const ViewPastEvent = ()=>{
             >
               <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path>
             </svg>
-            <span className="text-4xl md:text-5xl relative">Photography</span>
+            <span className="text-4xl md:text-5xl relative">{initial.EventName}</span>
           </span>
         </h1>
       </main>
@@ -125,17 +127,25 @@ const ViewPastEvent = ()=>{
             </div>
            
             
-            
+            {(initial.ChiefGuest != undefined)?(<></>):(<>
             <div className='flex flex-col justify-start w-full my-4 text-start font-semibold'>
               <h1 className="font-bold bg-pink-500 px-4 py-1 rounded-md mb-2 text-lg text-white">
                 Chief Guests:
               </h1>
+          {initial.ChiefGuest.map((info)=>{
+
+            return(
+              <>
               <h2 className=' inline-flex items-center gap-x-2'><BsAward />
- Mr. Vedansh Jain</h2>
- <h2 className='inline-flex items-center gap-x-2'><BsAward />
- Mr. Vedansh Jain</h2>
+              {info}</h2>
+              
+              </>
+            )
+          })}
+
 
             </div>
+            </>)}
           </div>
           <div className="lg:max-w-lg lg:w-full -mt-14 sm:-mt-0 md:w-1/2 w-5/6">
             <img
