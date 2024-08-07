e147e921861c50b8efdd14799fb33ee65103b5ff
shreyash jain <sheryash123jain@gmail.com>
2024-08-07 22:27:02 +0530

    add loder

--- frontend/src/page/PhotoGallery.jsx
+++ frontend/src/page/PhotoGallery.jsx
@@ -23,7 +23,7 @@ const PhotoGallery = () => {
         { id: 7, url: sa7 },
       ];
       const [myPhotos,setMyPhotos] = useState([]);
-
+      const [loading,setLoading] = useState(true);
       const getMyPhotos = async()=>{
         try {
           const result = await axios.get(`https://backendsufal-shreyash-sanghis-projects.vercel.app/get_my_photo`);
@@ -40,8 +40,9 @@ const PhotoGallery = () => {
           );
           setMyPhotos(photoData); 
          }
-       
+       setLoading(false)
         } catch (error) {
+          setLoading(false)
           alert(error);
         }
       }
@@ -69,6 +70,72 @@ Photo
     ))}
     </>):(<></>)}
   </div> */}
+      {(loading)?(<>
+    <div className="flex flex-col gap-10"> 
+<div className=" flex flex-wrap mt-10 justify-evenly ">
+<div role="status" class="flex items-center justify-center h-56 w-[40%] sm:w-[20%] bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+    <svg class="w-10 h-10 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z"/>
+  </svg>
+    <span class="sr-only">Loading...</span>
+</div>
+<div role="status" class="flex items-center justify-center h-56 w-[40%] sm:w-[20%] bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+    <svg class="w-10 h-10 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z"/>
+  </svg>
+    <span class="sr-only">Loading...</span>
+</div>
+<div role="status" class="hidden sm:flex items-center justify-center h-56 w-[40%] sm:w-[20%] bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+    <svg class="w-10 h-10 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z"/>
+  </svg>
+    <span class="sr-only">Loading...</span>
+</div>
+<div role="status" class="hidden sm:flex items-center justify-center h-56 w-[40%] sm:w-[20%] bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+    <svg class="w-10 h-10 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z"/>
+  </svg>
+    <span class="sr-only">Loading...</span>
+</div>
+
+</div>
+<div className=" flex flex-wrap mt-10 justify-evenly ">
+<div role="status" class="flex items-center justify-center h-56 w-[40%] sm:w-[20%] bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+    <svg class="w-10 h-10 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z"/>
+  </svg>
+    <span class="sr-only">Loading...</span>
+</div>
+<div role="status" class="flex items-center justify-center h-56 w-[40%] sm:w-[20%] bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+    <svg class="w-10 h-10 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z"/>
+  </svg>
+    <span class="sr-only">Loading...</span>
+</div>
+<div role="status" class="hidden sm:flex items-center justify-center h-56 w-[40%] sm:w-[20%] bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+    <svg class="w-10 h-10 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z"/>
+  </svg>
+    <span class="sr-only">Loading...</span>
+</div>
+<div role="status" class="hidden sm:flex items-center justify-center h-56 w-[40%] sm:w-[20%] bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+    <svg class="w-10 h-10 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z"/>
+  </svg>
+    <span class="sr-only">Loading...</span>
+</div>
+
+</div>
+</div> 
+      </>):(<>
   <div className='gallery py-10 sm:py-6 md:py-2 px-10 sm:px-16'>
     {(myPhotos.length>0)?(<>
     {myPhotos.map(image => (
@@ -78,6 +145,7 @@ Photo
     ))}
     </>):(<></>)}
   </div>
+  </>)}
   <div className="h-20"></div>
   <Footer></Footer></>
    
