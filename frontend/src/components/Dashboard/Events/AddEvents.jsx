3922a31b2fd6a493694ce72ab95ba4f0e81df73a
Vedansh Jain <vedansh.code@gmail.com>
2025-09-06 15:54:31 +0530

    Update AddEvents.jsx Limit Removed Event Description

--- frontend/src/components/Dashboard/Events/AddEvents.jsx
+++ frontend/src/components/Dashboard/Events/AddEvents.jsx
@@ -393,7 +393,6 @@ const AddEvent = () => {
                     onChange={EventData}
                     autoComplete="off"
                     id="floating_password"
-                    maxLength={201}
                     className=" event-input block min-h-24  py-1.5 px-0 w-full   bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
                   />
